3bc5bfc7359801178712c55bdc039a7573aa27eb
Josue <jfr@iventiva.com>
2023-05-22 19:35:17 -0400

    Adjust user transactions Get method to filter according to the user role

--- src/api/user-transactions/user-transactions.controller.ts
+++ src/api/user-transactions/user-transactions.controller.ts
@@ -1,6 +1,7 @@
 import { ResponseToolkit } from "hapi";
 import { IGetAllUserTransactionsRequest } from "./interfaces/user-transaction-request-interfaces";
 import { UserTransactionsService } from "./user-transactions.service";
+import { Role } from "../shared/enums/role.enum";
 
 export class UserTransactionsController {
   private readonly userTransactionsService: UserTransactionsService;
@@ -10,9 +11,13 @@ export class UserTransactionsController {
   }
 
   async getAll(request: IGetAllUserTransactionsRequest, h: ResponseToolkit) {
-    const userTransactions = await this.userTransactionsService.getAll(
-      request.query
-    );
+    const isAdmin = (request.auth.credentials.scope || []).includes(Role.ADMIN);
+    let filter = request.query;
+
+    if (!isAdmin)
+      filter = { ...filter, userId: request.auth.credentials.id as any };
+
+    const userTransactions = await this.userTransactionsService.getAll(filter);
 
     return h.response(userTransactions);
   }
---
+++ src/db/migrations/1684786757860-AddInitialData.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import { Role } from "../../api/shared/enums/role.enum";
+import User from "../../api/users/entities/user.entity";
+import Event from "../../api/events/entities/event.entity";
+import { EventSport } from "../../api/shared/enums/event-sports.enum";
+import Bet from "../../api/bets/entities/bet.entity";
+import { EventScore } from "../../api/events/enums/event-score.enum";
+import { encryptPassword } from "../../utils/user.utils";
+
+export class AddInitialData1684786757860 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const encryptedPassword = await encryptPassword("admin");
+    const adminUser = User.merge(new User(), {
+      role: Role.ADMIN,
+      firstName: "admin",
+      email: "admin@mail.com",
+      password: encryptedPassword,
+      username: "admin",
+    });
+
+    const championsLeagueFinal = Event.merge(new Event(), {
+      firstTeam: "Manchester City",
+      secondTeam: "Inter de Milan",
+      sport: EventSport.SOCCER,
+    });
+
+    const uefaFinal = Event.merge(new Event(), {
+      firstTeam: "Sevilla",
+      secondTeam: "Roma",
+      sport: EventSport.SOCCER,
+    });
+
+    // Create first admin user
+    await queryRunner.manager.save(adminUser);
+
+    const firstEvent = await queryRunner.manager.save(championsLeagueFinal);
+    const secondEvent = await queryRunner.manager.save(uefaFinal);
+
+    // Creating bets
+    for (const event of [firstEvent, secondEvent]) {
+      const scores = Object.values(EventScore);
+      for (const score of scores) {
+        const bet = Bet.merge(new Bet(), {
+          option: scores.indexOf(score),
+          name: this.getBetName(score, event),
+          odd: this.genRand(),
+          sport: event.sport,
+          eventId: event.id,
+        });
+
+        await queryRunner.manager.save(bet);
+      }
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+
+  private getBetName(score: EventScore, event: Event): string {
+    if (score == EventScore.DRAW) return "Draw";
+    const teamWinner =
+      score == EventScore.FIRST_TEAM_WON ? event.firstTeam : event.secondTeam;
+
+    return `${teamWinner} WIN`;
+  }
+
+  private genRand(min = 0, max = 2, decimalPlaces = 2) {
+    const rand = Math.random() * (max - min) + min;
+    const power = Math.pow(10, decimalPlaces);
+    return Math.floor(rand * power) / power;
+  }
+}
